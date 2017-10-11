bbe3d6c98462241736a8c37c5ec1a3b02db2daea
Cameron Nicklaus <camnicklaus@gmail.com>
2017-10-10 20:35:40 -0700

    removed api kay

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_KEY = process.env.API_KEY;
-const API_KEY = 'd5fb03866b025ebec0aeadc187674766';
+const API_KEY = process.env.API_KEY;
+
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
