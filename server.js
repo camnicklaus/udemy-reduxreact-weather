ebb172e78227612035f6d8dd11d6671957b30116
Cameron Nicklaus <camnicklaus@gmail.com>
2017-10-10 17:24:47 -0700

    update server route

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ var path = require('path');
 var port = process.env.PORT || 3000;
 var app = express();
 
-app.use(express.static(path.join(__dirname, './')));
+app.use(express.static(path.join(__dirname)));
 
 app.get('*', function(request, response) {
-    response.sendFile(path.resolve(__dirname, './', 'index.html'));
+    response.sendFile(path.resolve(__dirname, 'index.html'));
 }).listen(port, function() {
     console.log('listening on port ' + port);
 });
