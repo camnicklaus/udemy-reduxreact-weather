2c10cf613a97695ab536d34e89ed5f0e10071883
Cameron Nicklaus <camnicklaus@gmail.com>
2017-10-10 20:53:45 -0700

    debugging openweather http (no s)

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_KEY = process.env.API_KEY;
 
-const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://crossorigin.me/http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
